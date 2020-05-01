16b9fd8eb713742d28d0186c820489c273da2af1
Shannon Nabors <shannon.nabors@gmail.com>
2020-05-01 11:54:03 -0400

    Fixed accuracy issue, now two separate sets of three numbers can be combined to get an answer. Also fixed it so that two of the same number won't break everything

--- different_countdown.rb
+++ different_countdown.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require 'benchmark'
 
 $goal = 0
 $numbers = []
@@ -19,12 +20,15 @@ def get_goal
 end
 
 def run
-    $goal = 870#get_goal
-    $numbers = [100, 75, 50, 25, 9, 1]#get_numbers(4, 2).sort.reverse
+    $goal = get_goal
+    $numbers = get_numbers(4, 2).sort.reverse
+    puts ""
     puts "GOAL: ", $goal
+    puts ""
     puts "NUMBERS: ", $numbers
 
     separate($numbers)
+    add_three_sets
     solve
 end
 
@@ -32,10 +36,10 @@ $pairs = {}
 $solutions = []
 
 OPS = {
-    "+": Proc.new { |a, b| a + b },
-    "-": Proc.new { |a, b| (a != b && a-b != b) ? a - b : false },
-    "*": Proc.new { |a, b| a > 1 && b > 1 ? a * b : false },
-    "/": Proc.new { |a, b| (a % b == 0 && b != 1 && a/b != b) ? a / b : false }
+    "+": Proc.new { |a, b| a && b ? a + b : false },
+    "-": Proc.new { |a, b| (a && b && a != b && a-b != b && a>b) ? a - b : false },
+    "*": Proc.new { |a, b| a && b && a > 1 && b > 1 ? a * b : false },
+    "/": Proc.new { |a, b| (a && b && a % b == 0 && b != 1 && a/b != b && a>=b) ? a / b : false }
 }
 
 
@@ -45,10 +49,10 @@ def separate(nums)
         while index < nums.length
             a, b, results = n, nums[index], {}
 
-            results["+"] = OPS[:+].call(a, b)
-            results["-"] = OPS[:-].call(a, b)
-            results["*"] = OPS[:*].call(a, b)
-            results["/"] = OPS[:/].call(a, b)
+            results["#{a}+#{b}"] = OPS[:+].call(a, b)
+            results["#{a}-#{b}"] = OPS[:-].call(a, b)
+            results["#{a}*#{b}"] = OPS[:*].call(a, b)
+            results["#{a}/#{b}"] = OPS[:/].call(a, b)
 
             $pairs[[a, b]] = results
 
@@ -57,13 +61,89 @@ def separate(nums)
     end
 end
 
+def add_three_sets
+    three_sets = []
+    
+    $pairs.each do |pair, possibilities|
+        if $numbers.count(pair[0]) > 1
+            pool = $numbers.clone()
+            pair.each {|num| pool.delete_at(pool.index(num))}
+        elsif $numbers.count(pair[1]) > 1
+            pool = $numbers.clone()
+            pair.each {|num| pool.delete_at(pool.index(num))}
+        else
+            pool = $numbers - pair
+        end
+
+        pool.each do |num|
+            if num <= pair[1]
+                three_set = pair.clone() << num
+                three_sets << three_set
+            end
+        end
+    end
+    
+    three_sets = three_sets.uniq
+    # Please for the love of god come back and find a better way to do this
+    
+    three_sets.each do |set|
+        a, b, c, set_possibilities = set[0], set[1], set[2], {}
+        $pairs[set] = set_possibilities
+        set_possibilities["#{a}+#{b}+#{c}"] = OPS[:+].call(OPS[:+].call(a, b), c)
+        set_possibilities["#{a}+#{b}-#{c}"] = OPS[:-].call(OPS[:+].call(a, b), c)
+        set_possibilities["#{a}-#{b}+#{c}"] = OPS[:+].call(OPS[:-].call(a, b), c)
+        set_possibilities["#{a}-#{b}-#{c}"] = OPS[:-].call(OPS[:-].call(a, b), c)
+        set_possibilities["#{b}+#{c}-#{a}"] = OPS[:-].call(OPS[:+].call(b, c), a)
+        set_possibilities["(#{a}+#{b})*#{c}"] = OPS[:*].call(OPS[:+].call(a, b), c)
+        set_possibilities["#{a}+(#{b}*#{c})"] = OPS[:+].call(a, OPS[:*].call(c, b))
+        set_possibilities["(#{a}+#{b})/#{c}"] = OPS[:/].call(OPS[:+].call(a, b), c)
+        set_possibilities["#{a}+(#{b}/#{c})"] = OPS[:+].call(a, OPS[:/].call(b, c))
+        set_possibilities["(#{a}-#{b})/#{c}"] = OPS[:/].call(OPS[:-].call(a, b), c)
+        set_possibilities["#{a}-(#{b}/#{c})"] = OPS[:-].call(a, OPS[:/].call(b, c))
+        set_possibilities["(#{a}-#{b})*#{c}"] = OPS[:*].call(OPS[:-].call(a, b), c)
+        set_possibilities["#{a}-(#{b}*#{c})"] = OPS[:-].call(a, OPS[:*].call(b, c))
+        set_possibilities["(#{a}*#{b})+#{c}"] = OPS[:+].call(OPS[:*].call(a, b), c)
+        set_possibilities["#{a}*(#{b}+#{c})"] = OPS[:*].call(a, OPS[:+].call(b, c))
+        set_possibilities["(#{a}*#{b})-#{c}"] = OPS[:-].call(OPS[:*].call(a, b), c)
+        set_possibilities["#{a}*(#{b}-#{c})"] = OPS[:*].call(a, OPS[:-].call(b, c))
+        set_possibilities["(#{a}/#{b})+#{c}"] = OPS[:+].call(OPS[:/].call(a, b), c)
+        set_possibilities["#{a}/(#{b}+#{c})"] = OPS[:/].call(a, OPS[:+].call(b, c))
+        set_possibilities["(#{a}/#{b})-#{c}"] = OPS[:-].call(OPS[:/].call(a, b), c)
+        set_possibilities["#{a}/(#{b}-#{c})"] = OPS[:/].call(a, OPS[:-].call(b, c))
+        set_possibilities["(#{a}+#{c})/#{b}"] = OPS[:/].call(OPS[:+].call(a, c), b)
+        set_possibilities["(#{a}-#{c})/#{b}"] = OPS[:/].call(OPS[:-].call(a, c), b)
+        set_possibilities["(#{a}+#{c})*#{b}"] = OPS[:*].call(OPS[:+].call(a, c), b)
+        set_possibilities["(#{a}-#{c})*#{b}"] = OPS[:*].call(OPS[:-].call(a, c), b)
+        set_possibilities["(#{a}*#{c})+#{b}"] = OPS[:+].call(OPS[:*].call(a, c), b)
+        set_possibilities["(#{a}/#{c})+#{b}"] = OPS[:+].call(OPS[:/].call(a, c), b)
+        set_possibilities["(#{a}*#{c})-#{b}"] = OPS[:-].call(OPS[:*].call(a, c), b)
+        set_possibilities["(#{a}/#{c})-#{b}"] = OPS[:-].call(OPS[:/].call(a, c), b)
+        set_possibilities["(#{b}+#{c})/#{a}"] = OPS[:/].call(OPS[:+].call(b, c), a)
+        set_possibilities["(#{b}*#{c})-#{a}"] = OPS[:-].call(OPS[:*].call(b, c), a)
+        set_possibilities["#{b}-(#{a}/#{c})"] = OPS[:-].call(b, OPS[:/].call(a, c))
+        set_possibilities["#{b}/(#{a}-#{c})"] = OPS[:/].call(b, OPS[:-].call(a, c))
+        set_possibilities["#{c}-(#{a}/#{b})"] = OPS[:-].call(c, OPS[:/].call(a, b))
+        set_possibilities["#{c}/(#{a}-#{b})"] = OPS[:/].call(c, OPS[:-].call(a, b))
+        set_possibilities["#{a}*#{b}*#{c}"] = OPS[:*].call(OPS[:*].call(a, b), c)
+        set_possibilities["#{a}/#{b}/#{c}"] = OPS[:/].call(OPS[:/].call(a, b), c)
+        set_possibilities["#{a}/#{b}*#{c}"] = OPS[:*].call(OPS[:/].call(a, b), c)
+        set_possibilities["#{a}*#{b}/#{c}"] = OPS[:/].call(OPS[:*].call(a, b), c)
+        set_possibilities["#{b}/#{c}*#{a}"] = OPS[:*].call(OPS[:/].call(b, c), a)
+    end
+end
+
 def possible_operands(set)
     operands = (set.length < 6 ? set.clone() : [])
+    if set.length == 3
+        operands << set
+    end
     set.each_with_index do |n, i|
         index = i + 1
         while index < set.length
             a, b = n, set[index]
-            operands << [a, b]
+            if !operands.include?([a, b])
+                operands << [a, b]
+            end
             index += 1
         end
     end
@@ -71,14 +151,19 @@ def possible_operands(set)
 end
 
 def hanging_plus_or_minus(solution_string)
+    # returns true if solution needs to be wrapped in ()
     return false if (!solution_string.include?("+") && !solution_string.include?("-"))
     return true if !solution_string.include?(")")
+
     reversed = solution_string.reverse
     plus_index = reversed.index("+")
     minus_index = reversed.index("-")
-    paren_index = reversed.index(")")
-    hanging_plus = (plus_index && plus_index < paren_index)
-    hanging_minus = (minus_index && minus_index < paren_index)
+    close_index = reversed.index(")")
+    open_index = reversed.index("(")
+
+    hanging_plus = (plus_index && ((plus_index < close_index && plus_index < open_index) || (plus_index > close_index && plus_index > open_index)))
+    hanging_minus = (minus_index && ((minus_index < close_index && minus_index < open_index) || (minus_index > close_index && minus_index > open_index)))
+
     return true if (hanging_plus || hanging_minus)
     return false
 end
@@ -117,7 +202,6 @@ def try_all_ops(number_one, number_two, solution, pool, addendum)
         end
 
         if new_number == $goal
-            # binding.pry
             $solutions << solution
         end
 
@@ -130,37 +214,71 @@ def try_all_ops(number_one, number_two, solution, pool, addendum)
 end
 
 def exhaust_pool(pool, num, solution)
+
     set_of_combos = possible_operands(pool)
+
     set_of_combos.each do |combo|
         if combo.class == Integer
-            pool = pool - [combo]
+
+            if pool.count(combo) > 1
+                pool.delete_at(pool.index(combo))
+            else
+                pool = pool - [combo]
+            end
+
             try_all_ops(num, combo, solution, pool, false)
             pool << combo
             pool = pool.sort.reverse
+
         elsif combo.class == Array
-            pool = pool - combo
-            $pairs[combo].each do |operator, number|
+            combo = combo.sort.reverse
+            # please fix
+
+            if pool == combo
+                pool = []
+            elsif $numbers.count(combo[0]) > 1
+                pool.delete_at(pool.index(combo[0]))
+                pool = pool - [combo[1]]
+            elsif $numbers.count(combo[1]) > 1
+                pool.delete_at(pool.index(combo[1]))
+                pool = pool - [combo[0]]
+            else
+                pool = pool - combo
+            end
+
+            $pairs[combo].each do |op_string, number|
                 if number
-                    addendum = "(" + combo[0].to_s + operator + combo[1].to_s + ")"
+                    addendum = "(" + op_string + ")"
                     try_all_ops(num, number, solution, pool, addendum)
                 end
             end
+
             pool << combo
             pool = pool.flatten.sort.reverse
+
         end
     end
 end
 
 def solve
-    # binding.pry
     $pairs.each do |pair, possibilities|
-        possibilities.each do |op, num|
+
+        possibilities.each do |op_string, num|
             if num
-                pool = $numbers - pair
-                solution = pair.join(op)
-                # if op == "+" || op == "-"
-                #     solution = "(" + solution + ")"
-                # end
+                if pair.count(pair[1]) > 1
+                    pool = $numbers.clone()
+                    pair.each {|num| pool.delete_at(pool.index(num))}
+                elsif $numbers.count(pair[0]) > 1
+                    pool = $numbers.clone()
+                    pair.each {|num| pool.delete_at(pool.index(num))}
+                elsif $numbers.count(pair[1]) > 1
+                    pool = $numbers.clone()
+                    pair.each {|num| pool.delete_at(pool.index(num))}
+                else
+                    pool = $numbers - pair
+                end
+
+                solution = op_string
                 if num == $goal
                     $solutions << solution
                     next
@@ -170,10 +288,17 @@ def solve
         end
     end
 
-    $solutions.each do |solution|
+    puts ""
+    puts "SOLUTIONS:"
+
+    $solutions.uniq.each do |solution|
         puts solution
     end
-    # binding.pry
+
+    puts ""
+    puts "TIME:"
+
 end
 
-run
+puts Benchmark.realtime { run }
+puts ""
